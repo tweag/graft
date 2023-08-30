ff8dd9d3c66e1ed2359938fcae66fd60dc4ff9b5
mmontin <mathieu.montin@tweag.io>
2023-08-30 16:53:31 +0200

    up to a trace, though it behaves improperly

--- src/Examples/Ltl/Accounts.hs
+++ src/Examples/Ltl/Accounts.hs
@@ -1,18 +1,33 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Examples.Ltl.Accounts where
 
+import Control.Applicative
 import Control.Monad.Except
-import Control.Monad.Identity
 import Control.Monad.State
+import Data.Map (Map)
 import Data.Map qualified as Map
+import Effect.Error
+import Effect.Error.Passthrough ()
+import Effect.TH
+import Logic.Ltl
+import Logic.SingleStep
 
 type Payment = (String, Integer, String)
 
 type Validator = Payment -> Integer -> String -> Bool
 
-type Register = Map.Map String (Integer, Validator)
+type Register = Map String (Integer, Validator)
 
 class (Monad m) => MonadAccounts m where
   addUser :: String -> Validator -> Integer -> m ()
@@ -25,12 +40,19 @@ data AccountsError
   | PaymentRefused
   deriving (Show)
 
-type Accounts = ExceptT AccountsError (StateT Register Identity)
+type AccountsT m = ExceptT AccountsError (StateT Register m)
 
-runAccounts :: Accounts a -> (Either AccountsError a, Register)
-runAccounts = runIdentity . flip runStateT Map.empty . runExceptT
+instance {-# OVERLAPPING #-} (MonadPlus m) => Alternative (AccountsT m) where
+  empty = lift mzero
+  ExceptT (StateT f) <|> ExceptT (StateT g) =
+    ExceptT . StateT $ \s -> f s `mplus` g s
 
-instance MonadAccounts Accounts where
+instance {-# OVERLAPPING #-} (MonadPlus m) => MonadPlus (AccountsT m)
+
+runAccountsT :: AccountsT m a -> m (Either AccountsError a, Register)
+runAccountsT = flip runStateT Map.empty . runExceptT
+
+instance (Monad m) => MonadAccounts (AccountsT m) where
   addUser name val balance = do
     user <- gets (Map.lookup name)
     case user of
@@ -57,8 +79,8 @@ instance MonadAccounts Accounts where
 -- Expectations: "validatorAlwaysReceives" will only accept payments
 -- that increase the balance of "me"
 --
--- Bug: since it is possible to exchange negative amounts, being the
--- actual recipient is not sufficient.
+-- Reality: since it is possible to exchange negative amounts, being
+-- the actual recipient is not sufficient.
 validatorAlwaysReceives :: Validator
 validatorAlwaysReceives (_, _, recipient) _ me = recipient == me
 
@@ -68,8 +90,8 @@ validatorAcceptsAll _ _ _ = True
 -- Expectations: "validatorNeverNegative" will never accept payments
 -- that leave the balance negative.
 --
--- Bug: balance should be checked to be positive after the payment has
--- been made, not before.
+-- Reality: balance should be checked to be positive after the payment
+-- has been made, not before.
 validatorNeverNegative :: Validator
 validatorNeverNegative _ bal _ = bal > 0
 
@@ -80,5 +102,52 @@ scenario = do
   addUser "judith" validatorNeverNegative 500
   issuePayment ("bob", 400, "judith")
   issuePayment ("bob", 600, "alice")
-  issuePayment ("judith", 1000, "alice")
+  issuePayment ("judith", 600, "alice")
   getBalance "judith"
+
+defineEffectType ''MonadAccounts
+makeEffect ''MonadAccounts ''MonadAccountsEffect
+
+data AccountsMod where
+  AccountsMod :: (Payment -> Maybe Payment) -> AccountsMod
+
+instance Semigroup AccountsMod where
+  (AccountsMod f1) <> (AccountsMod f2) =
+    AccountsMod
+      (\x -> (f1 <=< f2) x <|> f1 x <|> f2 x)
+
+instance
+  (MonadError AccountsError m, MonadAccounts m) =>
+  InterpretMod AccountsMod m MonadAccountsEffect
+  where
+  interpretMod (IssuePayment payment) (Just (AccountsMod modif)) = AttemptModification $
+    case modif payment of
+      Nothing -> return Nothing
+      (Just newPayment) -> Just <$> issuePayment newPayment
+  interpretMod (IssuePayment _) Nothing = SkipModification
+  interpretMod _ _ = PassModification
+
+paymentsInvolvingXMod :: String -> Bool -> (Integer -> Maybe Integer) -> AccountsMod
+paymentsInvolvingXMod target isRecipient change =
+  AccountsMod
+    ( \(sender, amount, recipient) -> case change amount of
+        Nothing -> Nothing
+        Just x ->
+          if target == (if isRecipient then recipient else sender)
+            then Just (sender, x, recipient)
+            else Nothing
+    )
+
+increaseJudithPaymentsMod :: AccountsMod
+increaseJudithPaymentsMod =
+  paymentsInvolvingXMod "judith" False (\x -> Just (x + 500))
+
+interpretAndRun ::
+  LtlAST AccountsMod '[MonadAccountsEffect, MonadErrorEffect AccountsError] a ->
+  [(Either AccountsError a, Register)]
+interpretAndRun =
+  runAccountsT
+    . interpretLtlAST
+      @'[ InterpretModTag,
+          InterpretEffectStatefulTag
+        ]
