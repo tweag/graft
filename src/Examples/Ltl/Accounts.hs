ebda52844d8954488740ad79b58f5367562b92ef
mmontin <mathieu.montin@tweag.io>
2023-09-01 18:19:56 +0200

    updating Account example to be richer

--- src/Examples/Ltl/Accounts.hs
+++ src/Examples/Ltl/Accounts.hs
@@ -15,76 +15,128 @@ module Examples.Ltl.Accounts where
 import Control.Applicative
 import Control.Monad.Except
 import Control.Monad.State
+import Data.Bifunctor (second)
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Set (Set)
+import Data.Set qualified as Set
 import Effect.Error
 import Effect.Error.Passthrough ()
 import Effect.TH
 import Logic.Ltl
 import Logic.SingleStep
 
+-- * Depiction of the domain
+
+-- | A payment contains a sender, an amount and a recipient
 type Payment = (String, Integer, String)
 
-type Validator = Payment -> Integer -> String -> Bool
+-- | The type for validators
+type Policy =
+  -- | The payment currently validated
+  Payment ->
+  -- | The funds possessed by the current user
+  Integer ->
+  -- | The current user for which validation is called
+  String ->
+  -- | Returns whether the payment should go through
+  Bool
 
-type Register = Map String (Integer, Validator)
+-- | The register associates:
+--
+-- - policy names with policies
+--
+-- - users with a balance and a set of policy names
+data Register = Register
+  { policies :: Map String Policy,
+    accounts :: Map String (Integer, Set String)
+  }
 
+initialRegister :: Register
+initialRegister = Register Map.empty Map.empty
+
+-- | Our domain specification: users can be added, payments can be
+-- attempted and balances can be requested.
 class (Monad m) => MonadAccounts m where
-  addUser :: String -> Validator -> Integer -> m ()
+  addUser :: String -> Integer -> m ()
+  addPolicy :: String -> Policy -> m ()
+  subscribeToPolicy :: String -> String -> m ()
   issuePayment :: Payment -> m ()
   getBalance :: String -> m Integer
 
+-- | Errors raised by the domain
 data AccountsError
   = NoSuchAccount String
   | AlreadyExistingAccount String
-  | PaymentRefused
+  | NoSuchPolicy String
+  | AlreadyExistingPolicy String
+  | PolicyError
   deriving (Show)
 
+-- | Our domain implementation
 type AccountsT m = ExceptT AccountsError (StateT Register m)
 
-instance {-# OVERLAPPING #-} (MonadPlus m) => Alternative (AccountsT m) where
-  empty = lift mzero
-  ExceptT (StateT f) <|> ExceptT (StateT g) =
-    ExceptT . StateT $ \s -> f s `mplus` g s
-
-instance {-# OVERLAPPING #-} (MonadPlus m) => MonadPlus (AccountsT m)
-
-runAccountsT :: AccountsT m a -> m (Either AccountsError a, Register)
-runAccountsT = flip runStateT Map.empty . runExceptT
+-- | A function to run the domain into the list monad and return
+-- relevant information
+runAccountsT :: AccountsT [] a -> [(Either AccountsError a, Map String (Integer, Set String))]
+runAccountsT =
+  map (second accounts)
+    . flip runStateT initialRegister
+    . runExceptT
 
 instance (Monad m) => MonadAccounts (AccountsT m) where
-  addUser name val balance = do
-    user <- gets (Map.lookup name)
-    case user of
+  addUser name balance = do
+    accs <- gets accounts
+    case Map.lookup name accs of
       Just _ -> throwError $ AlreadyExistingAccount name
-      Nothing -> modify $ Map.insert name (balance, val)
-  issuePayment t@(sender, amount, recipient) = do
-    senderInfos <- gets (Map.lookup sender)
-    recipientInfos <- gets (Map.lookup recipient)
-    case (senderInfos, recipientInfos) of
-      (Nothing, _) -> throwError $ NoSuchAccount sender
-      (_, Nothing) -> throwError $ NoSuchAccount recipient
-      (Just (sBal, sVal), Just (rBal, rVal)) ->
-        if sVal t sBal sender && rVal t rBal recipient
-          then do
-            modify $ Map.insert sender (sBal - amount, sVal)
-            modify $ Map.insert recipient (rBal + amount, rVal)
-          else throwError PaymentRefused
+      Nothing -> modify $ \x -> x {accounts = Map.insert name (balance, Set.empty) accs}
+  addPolicy name val = do
+    pols <- gets policies
+    case Map.lookup name pols of
+      Just _ -> throwError $ AlreadyExistingPolicy name
+      Nothing -> modify $ \x -> x {policies = Map.insert name val pols}
+  subscribeToPolicy userName polName = do
+    Register pols accs <- get
+    case Map.lookup userName accs of
+      Nothing -> throwError $ NoSuchAccount userName
+      Just (bal, accPols) ->
+        if Map.member polName pols
+          then modify $ \x -> x {accounts = Map.insert userName (bal, Set.insert polName accPols) accs}
+          else throwError $ NoSuchPolicy polName
   getBalance name = do
-    user <- gets (Map.lookup name)
-    case user of
-      Just (bal, _) -> return bal
+    accs <- gets accounts
+    case Map.lookup name accs of
       Nothing -> throwError $ NoSuchAccount name
+      Just (bal, _) -> return bal
+  issuePayment payment@(sender, amount, recipient) = do
+    Register pols accs <- get
+    case Map.lookup sender accs of
+      Nothing -> throwError $ NoSuchAccount sender
+      Just (senderBal, senderPols) -> case Map.lookup recipient accs of
+        Nothing -> throwError $ NoSuchAccount recipient
+        Just (recipientBal, recipientPols) -> do
+          sPols <- foldM (\current el -> (\f -> f payment senderBal sender && current) <$> getPolicy pols el) True senderPols
+          unless sPols $ throwError PolicyError
+          rPols <- foldM (\current el -> (\f -> f payment recipientBal recipient && current) <$> getPolicy pols el) True recipientPols
+          unless rPols $ throwError PolicyError
+          let accs' = Map.insert sender (senderBal - amount, senderPols) accs
+          modify $ \x -> x {accounts = Map.insert recipient (recipientBal + amount, recipientPols) accs'}
+    where
+      getPolicy pols x = case Map.lookup x pols of
+        Nothing -> throwError $ NoSuchPolicy x
+        Just cPol -> return cPol
+
+-- * Some (possibly bugged) validators
 
 -- Expectations: "validatorAlwaysReceives" will only accept payments
 -- that increase the balance of "me"
 --
 -- Reality: since it is possible to exchange negative amounts, being
 -- the actual recipient is not sufficient.
-validatorAlwaysReceives :: Validator
+validatorAlwaysReceives :: Policy
 validatorAlwaysReceives (_, _, recipient) _ me = recipient == me
 
-validatorAcceptsAll :: Validator
+validatorAcceptsAll :: Policy
 validatorAcceptsAll _ _ _ = True
 
 -- Expectations: "validatorNeverNegative" will never accept payments
@@ -92,14 +144,22 @@ validatorAcceptsAll _ _ _ = True
 --
 -- Reality: balance should be checked to be positive after the payment
 -- has been made, not before.
-validatorNeverNegative :: Validator
+validatorNeverNegative :: Policy
 validatorNeverNegative _ bal _ = bal > 0
+
+-- * A few scenarios that exhibit no wrong behavior
 
 scenario :: (MonadAccounts m) => m Integer
 scenario = do
-  addUser "alice" validatorAlwaysReceives 0
-  addUser "bob" validatorAcceptsAll 1000
-  addUser "judith" validatorNeverNegative 500
+  addPolicy "alwaysReceives" validatorAlwaysReceives
+  addPolicy "acceptsAll" validatorAcceptsAll
+  addPolicy "neverNegative" validatorNeverNegative
+  addUser "alice" 0
+  subscribeToPolicy "alice" "alwaysReceives"
+  addUser "bob" 1000
+  subscribeToPolicy "bob" "acceptsAll"
+  addUser "judith" 500
+  subscribeToPolicy "judith" "neverNegative"
   issuePayment ("bob", 400, "judith")
   issuePayment ("bob", 600, "alice")
   issuePayment ("judith", 600, "alice")
@@ -137,9 +197,16 @@ conditionalPaymentMod condSender condRecipient change =
 increaseJudithPaymentsMod :: AccountsMod
 increaseJudithPaymentsMod = conditionalPaymentMod (== "judith") (const True) (Just . (+ 500))
 
+instance {-# OVERLAPPING #-} (MonadPlus m) => Alternative (AccountsT m) where
+  empty = lift mzero
+  ExceptT (StateT f) <|> ExceptT (StateT g) =
+    ExceptT . StateT $ \s -> f s `mplus` g s
+
+instance {-# OVERLAPPING #-} (MonadPlus m) => MonadPlus (AccountsT m)
+
 interpretAndRun ::
   LtlAST AccountsMod '[MonadAccountsEffect, MonadErrorEffect AccountsError] a ->
-  [(Either AccountsError a, Register)]
+  [(Either AccountsError a, Map String (Integer, Set String))]
 interpretAndRun =
   runAccountsT
     . interpretLtlAST
